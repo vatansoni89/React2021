96a550e55309e12c1df36c9697e88faa648082ea
Vatan Soni <vatan.soni.ext@sivantos.com>
2021-03-13 19:19:11 +0530

    two way binding

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -8,12 +8,13 @@ class App extends Component {
     persons: [
       { name: "Max", age: 28 },
       { name: "Vatan", age: 33 },
-      { name: "Tanu", age: 36 }
+      { name: "Tanu", age: 36 },
     ],
-    nations:[
-      {name: "India", pop: "2B"},
-      {name: "UK", pop: "1B"}
-    ]
+    nations: [
+      { name: "India", pop: "2B" },
+      { name: "UK", pop: "1B" },
+      { name: "USA", pop: "1B" }
+    ],
   };
 
   switchNameHandler = (newName) => {
@@ -22,7 +23,17 @@ class App extends Component {
       persons: [
         { name: "Max", age: 30 },
         { name: newName, age: 30 },
-        { name: "TanuJi", age: 30 }
+        { name: "TanuJi", age: 30 },
+      ],
+    });
+  };
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "Max", age: 30 },
+        { name: "Vatan", age: 30 },
+        { name: event.target.value, age: 30 },
       ],
     });
   };
@@ -37,20 +48,33 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <button onClick={this.switchNameHandler.bind(this,'MaxxFromBtn_bind')}>Click Me Bind!</button>
-        <button onClick={()=> this.switchNameHandler('MaxxFromBtn_Arrow')}>Click Me Arrow!</button>
+        <button onClick={this.switchNameHandler.bind(this, "MaxxFromBtn_bind")}>
+          Click Me Bind!
+        </button>
+        <button onClick={() => this.switchNameHandler("MaxxFromBtn_Arrow")}>
+          Click Me Arrow!
+        </button>
         <Person
-          age={this.state.persons[1].name}
-          name={this.state.persons[1].age}
+          age={this.state.persons[0].age}
+          name={this.state.persons[0].name}
           nation={this.state.nations[0].name}
-          click={this.switchNameHandler.bind(this,'MaxxFromProp')}
+          click={this.switchNameHandler.bind(this, "Maxx1FromProp")}
+        >
+          Beer!!!
+        </Person>
+        <Person
+          age={this.state.persons[1].age}
+          name={this.state.persons[1].name}
+          nation={this.state.nations[1].name}
+          click={this.switchNameHandler.bind(this, "Maxx2FromProp")}
         >
           Badminton
         </Person>
         <Person
-          age={this.state.persons[2].name}
-          name={this.state.persons[2].age}
-          nation={this.state.nations[1].name}
+          age={this.state.persons[2].age}
+          name={this.state.persons[2].name}
+          nation={this.state.nations[2].name}
+          changed={this.nameChangedHandler}
         >
           Meditation
         </Person>
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const person = (props) => {
-    return (<div onClick={props.click}>I m a person, age is {props.age} and name is {props.name}, nation : {props.nation} and my hobby is {props.children}</div>)
-}
+  return (
+    <div>
+      <div onClick={props.click}>
+        I m a person, age is {props.age} and name is {props.name}, nation :{" "}
+        {props.nation} and my hobby is {props.children}
+      </div>
+      <hr />
+      <div>
+        <input type="text" onChange={props.changed} value={props.name}></input>
+      </div>
+    </div>
+  );
+};
 
 export default person;
