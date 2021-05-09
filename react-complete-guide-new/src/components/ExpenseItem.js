ce042c6bad7a397fd611c25453070d0201caeed7
Vatan Soni <vatan.soni.ext@sivantos.com>
2021-05-09 19:35:09 +0530

    month-day-year formatted

--- react-complete-guide-new/src/components/ExpenseItem.js
+++ react-complete-guide-new/src/components/ExpenseItem.js
@@ -1,15 +1,25 @@
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
 
-let expenseItem = () => {
-  return (
-    <div className='expense-item'>
-      <div>March 28th 2021</div>
-      <div className='expense-item__description'>
-          <h2>Car Insurance</h2>
-          <div className='expense-item__price'>$294.67</div>
+let expenseItem = (props) => {
+  console.log("Prop is>>", props);
+  return props.expensesList.map((x) => {
+    let month = x.date.toLocaleString("en-US", { month: "long" });
+    let day = x.date.toLocaleString("en-US", { day: "2-digit" });
+    let year = x.date.getFullYear();
+    return (
+      <div className="expense-item">
+        <div>
+          <div>{month}</div>
+          <div>{year}</div>
+          <div>{day}</div>
+        </div>
+        <div className="expense-item__description">
+          <h2>{x.title}</h2>
+          <div className="expense-item__price">{x.amount}</div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  });
 };
 
 export default expenseItem;
