26dff70b36e806d859c706af00f5a89334d73565
Vatan Soni <vatan.soni.ext@sivantos.com>
2021-05-12 23:14:39 +0530

    issue fixed but prop lifetime is question

--- react-complete-guide-new/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide-new/src/components/Expenses/ExpenseItem.js
@@ -4,21 +4,13 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 let ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.item.title);
-  
-  const clickHandler = () => {
-    console.log('Title is frm item>>> ',props.item.title);
-    setTitle(props.item.title);
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.item.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.item.title}</h2>
         <div className="expense-item__price">{props.item.amount}</div>
       </div>
-      <button onClick={clickHandler}>Click Me!!</button>
     </Card>
   );
 };
