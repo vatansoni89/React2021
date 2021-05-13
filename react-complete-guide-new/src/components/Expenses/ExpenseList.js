51ea78363782bad4558c17d8cf06422b99c9201d
Vatan Soni <vatan.soni.ext@sivantos.com>
2021-05-13 19:12:59 +0530

    list updated with css

--- react-complete-guide-new/src/components/Expenses/ExpenseList.js
+++ react-complete-guide-new/src/components/Expenses/ExpenseList.js
@@ -1,18 +1,20 @@
 import ExpenseItem from "./ExpenseItem";
+import "./ExpenseList.css";
 
 const ExpenseList = (props) => {
-const filteredList = props.expensesList.filter(x=>x.date.getFullYear() == props.filteredYearOption);
-console.log('props.filteredYearOption',props.filteredYearOption);
+  const filteredList = props.expensesList.filter(
+    (x) => x.date.getFullYear() == props.filteredYearOption
+  );
+  
+  console.log("props.filteredYearOption", props.filteredYearOption);
   return (
     <div>
       {props.filteredYearOption == "All Year" ? (
         props.expensesList.map((x) => <ExpenseItem item={x} key={x.id} />)
       ) : filteredList.length == 0 ? (
-        <p>No Expense</p>
-      ) : (
-        filteredList.map((x) => (
-          <ExpenseItem item={x} key={x.id} />
-        ))
+        <h2 className="expenses-list__fallback">No Expense</h2>
+      ) : (<ul className='expenses-list'>
+        {filteredList.map((x) => <ExpenseItem item={x} key={x.id} />)}</ul>
       )}
     </div>
   );
